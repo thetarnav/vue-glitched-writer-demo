8c30489df3f363c27043455c28694f855a1ffacc
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2021-08-03 16:21:52 +0200

    vue-glitched-writer packege update to @1.2.2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,72 @@
 <template>
-  <div class="hello">
+	<div class="hello">
 		<GlitchedWriter tag="h1" :text="msg" appear />
-		<GlitchedWriter tag="p" :text="paragraph" :options="{html:true}" appear />
-  </div>
+		<GlitchedWriter
+			tag="p"
+			:text="paragraph"
+			:options="{ html: true }"
+			appear
+		/>
+		<GlitchedWriter
+			tag="p"
+			:text="texts"
+			:queue="{
+				loop: true,
+			}"
+			preset="encrypted"
+			appear
+		/>
+	</div>
 </template>
 
 <script>
+// import GlitchedWriter from '../../../vue-demi-gw/dist/vue-glitched-writer.esm'
 import GlitchedWriter from 'vue-glitched-writer'
 
+console.log(GlitchedWriter)
+
 export default {
-  name: 'HelloWorld',
-  components: {
-    GlitchedWriter
-  },
-  props: {
-    msg: String
-  },
-  setup(){
+	name: 'HelloWorld',
+	components: {
+		GlitchedWriter,
+	},
+	props: {
+		msg: String,
+	},
+	setup() {
 		const paragraph = `
 		For a guide and recipes on how to configure / customize this project, <br/>
       check out the <a href="https://github.com/thetarnav/vue-glitched-writer" target="_blank">vue glitched writer repo</a>.`
 
+		const texts = [
+			'First Text Of The Queue.',
+			'Second Item of the Queue!',
+			'Next up is...',
+			'THE LAST ONE',
+		]
+
 		return {
-			paragraph
+			paragraph,
+			texts,
 		}
-  }
+	},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
